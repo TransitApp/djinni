f49cf4a157d3194d472fb2abe056f47939573949
Julien <lepi.julien@gmail.com>
2024-01-08 13:03:54 -0500

    Add djinni utils objc files

---
+++ objc-lib/NSArray+Djinni.mm
@@ -0,0 +1,20 @@
+//
+//  NSArray+Djinni.mm
+//  Djinni
+//
+//  Created by Julien Lepine on 2024-01-08.
+//
+
+#import "NSArray+Djinni.h"
+
+@implementation NSArray (Djinni)
+
+- (NSUInteger)dynamicHash {
+    NSUInteger hash = 0;
+    for (NSObject *obj in self) {
+        hash ^= obj.hash;
+    }
+    return hash;
+}
+
+@end
