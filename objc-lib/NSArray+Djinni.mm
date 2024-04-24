003e9e5b94afff145377825c98e13ce54f865fa8
Julien <lepi.julien@gmail.com>
2024-04-24 18:43:38 -0400

    Switch to prime solution

--- objc-lib/NSArray+Djinni.mm
+++ objc-lib/NSArray+Djinni.mm
@@ -9,21 +9,15 @@
 
 @implementation NSArray (Djinni)
 
-/**
- * Based on: https://www.mikeash.com/pyblog/friday-qa-2010-06-18-implementing-equality-and-hashing.html
- */
-#define NSUINT_BIT (CHAR_BIT * sizeof(NSUInteger))
-#define NSUINTROTATE(val, howmuch) ((((NSUInteger)val) << howmuch) | (((NSUInteger)val) >> (NSUINT_BIT - howmuch)))
 - (NSUInteger)dynamicHash {
-    NSUInteger combinedHash = 0;
-    NSUInteger count = self.count;
-    for (NSUInteger i = 0; i < count; i++) {
-        id obj = self[i];
+    NSUInteger result = 1;
+    NSUInteger prime = 31;
+    for (id obj in self) {
         NSUInteger objHash = [obj respondsToSelector:@selector(dynamicHash)] ? [obj dynamicHash] : [obj hash];
-        NSUInteger rotationAmount = NSUINT_BIT / (i + 1);
-        combinedHash ^= NSUINTROTATE(objHash, rotationAmount);
+        result = prime * result + objHash;
     }
-    return combinedHash;
+    return result;
 }
+
 @end
 
