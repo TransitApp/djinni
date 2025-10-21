2f10473adb52e443ce3f9cd4c904504f733a1881
lepiju <lepi.julien@gmail.com>
2025-10-21 14:49:36 -0400

    Update comment

--- objc-lib/NSArray+Djinni.mm
+++ objc-lib/NSArray+Djinni.mm
@@ -9,12 +9,12 @@
 
 @implementation NSArray (Djinni)
 
+// Boost hash_combine pattern
 - (NSUInteger)dynamicHash {
     NSUInteger result = 0;
     for (id obj in self) {
         NSUInteger objHash = [obj respondsToSelector:@selector(dynamicHash)] ?
                             [obj dynamicHash] : [obj hash];
-        // Boost hash_combine pattern: distributes bits evenly to prevent collisions
         result ^= objHash + 0x9e3779b9 + (result << 6) + (result >> 2);
     }
     return result;
