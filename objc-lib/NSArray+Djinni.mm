bd060f9904576760c5f508cd6b2ac5a27f487098
lepiju <lepi.julien@gmail.com>
2025-10-21 14:44:38 -0400

    Use XOR-Based Hash Combining
    just like boost::hash_combine

--- objc-lib/NSArray+Djinni.mm
+++ objc-lib/NSArray+Djinni.mm
@@ -10,11 +10,12 @@
 @implementation NSArray (Djinni)
 
 - (NSUInteger)dynamicHash {
-    NSUInteger result = 1;
-    NSUInteger prime = 31;
+    NSUInteger result = 0;
     for (id obj in self) {
-        NSUInteger objHash = [obj respondsToSelector:@selector(dynamicHash)] ? [obj dynamicHash] : [obj hash];
-        result = prime * result + objHash;
+        NSUInteger objHash = [obj respondsToSelector:@selector(dynamicHash)] ?
+                            [obj dynamicHash] : [obj hash];
+        // Boost hash_combine pattern: distributes bits evenly to prevent collisions
+        result ^= objHash + 0x9e3779b9 + (result << 6) + (result >> 2);
     }
     return result;
 }
